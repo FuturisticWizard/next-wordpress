96228b0560c5ae3a7087af5ebac8ba2485f298f8
Konrad Czerniec <futuristicWizard@konradczerniec.space>
2024-02-23 13:02:31 +0000

    6. Create heading component, util functions, extend tailwind, and inner block

--- components/BlockRenderer/BlockRenderer.jsx
+++ components/BlockRenderer/BlockRenderer.jsx
@@ -1,12 +1,16 @@
 import { isNullableType } from 'graphql'
 import React from 'react'
 import { Cover } from '../Cover';
-
+import { Heading } from 'components/Heading';
 
 export const BlockRenderer = ({blocks}) => {
   return blocks.map( block => {
     switch (block.name) {
+        case "core/heading": {
+          return <Heading key={block.id} content={block.attributes.content} textAlign={block.attributes.textAlign} level={block.attributes.level}/>
+        }
         case "core/cover": {
+          console.log("BLOCK: ", block);
             return <Cover key={block.id} background={block.attributes.url}><BlockRenderer blocks={block.innerBlocks}/></Cover>
         }
         default: 
