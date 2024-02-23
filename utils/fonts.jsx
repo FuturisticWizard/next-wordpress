b89668410213a7c82911fb0d18a1a43208c8eb3c
Konrad Czerniec <futuristicWizard@konradczerniec.space>
2024-02-23 14:07:08 +0000

    7. Adding dynamic routes - [...slug].jsx

--- utils/fonts.jsx
+++ utils/fonts.jsx
@@ -6,18 +6,7 @@ export const getTextAlign = (textAlign="right") => {
     }
     return `${textAlignMap[textAlign] || ""}`;
 }
-// export const getFontSizeForHeading = (fontSize) => {
-//         console.log("fontsize detected : ", fontSize)
-//         const fontSizeMap = {
-//             'x-large': "text-6xl",
-//             large: "text-5xl",
-//             medium: "text-4xl",
-//             small: "text-3xl",
-//             5: "text-2xl",
-//             6: "text-xl",
-//         }
-//         return `${fontSizeMap[fontSize] || ""}`
-//     }
+
 export const getFontSizeForHeading = (level) => {
     const fontSizeMap = {
       1: "text-6xl",
