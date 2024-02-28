5d171ed87ee99a85301a36cf179832be9791122b
Konrad Czerniec <futuristicWizard@konradczerniec.space>
2024-02-28 17:48:04 +0000

    11. Query and clean wordpress mainMenu data to use on front-end

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import { gql } from "@apollo/client";
 import client from "client";
 import { BlockRenderer } from "components/BlockRenderer";
 import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
+import mapMainMenuItems from "utils/mapMainMenuItems";
 
 export default function Home(props) {
   console.log("PROPS: ", props)
@@ -18,11 +19,34 @@ export const getStaticProps = async () => {
           blocks(postTemplate: false)
         }
       }
+      acfOptionsMainMenu {
+        mainMenu {
+          menuItems {
+            menuItem {
+              destination {
+                ... on Page {
+                  uri
+                }
+              }
+              label
+            }
+            items {
+              destination {
+                 ... on Page {
+                  uri
+                }
+              }
+              label
+            }
+          }
+        }
+      }
     }
     `
   }) 
   return {
     props: {
+      mainMenuItems: mapMainMenuItems(data.acfOptionsMainMenu.mainMenu.menuItems),
       blocks: cleanAndTransformBlocks(data.nodeByUri.blocks) ,
       myexampleprop: "test"
     }
